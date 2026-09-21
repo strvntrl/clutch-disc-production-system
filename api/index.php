ed2d5d5cd6acacc4ec2f8231fe3ff50f8859ef85
YaXioz <alfianzyh@gmail.com>
2026-09-21 23:33:14 +0700

    test

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
 <?php
 
-$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-if ($requestUri === '/' || $requestUri === '') {
-    require __DIR__ . '/../index.php';
-    exit;
-}
-
-http_response_code(404);
-echo "404 - Page not found";
+echo "PHP Runtime Vercel berhasil berjalan!";
